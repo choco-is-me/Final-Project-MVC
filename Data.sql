253cf67d41da81131525de24837e751fba1f3057
thebao2307 <thebao1426@gmail.com>
2022-12-11 21:25:18 +0700

    data added

--- Data.sql
+++ Data.sql
@@ -12,10 +12,7 @@ CREATE TABLE [dbo].[Suppliers](
 	[SName] [varchar](50) NOT NULL,
 	[Phone] [varchar](50) NOT NULL,
 	[Email] [varchar](50) NOT NULL,
-	[Address1] [varchar](50) NOT NULL,
-	[Address2] [varchar](50) NULL,
-	[Suburb] [varchar](50) NOT NULL,
-	[State] [varchar](50) NOT NULL,
+	[Address] [varchar](50) NOT NULL,
 	[Postcode] [varchar](50) NOT NULL,
  CONSTRAINT [PK_Suppliers] PRIMARY KEY CLUSTERED 
 (
@@ -26,7 +23,11 @@ GO
 SET ANSI_PADDING OFF
 GO
 SET IDENTITY_INSERT [dbo].[Suppliers] ON
-INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address1], [Address2], [Suburb], [State], [Postcode]) VALUES (1, N'Bao Binh Co Ltd', N'(+84) 903 346 306', N'binhtranthanhbinh120@gmail.com', N'62 Phu Tho', N'62 Phu Tho', N'Ho Chi Minh',  N'Ho Chi Minh', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (1, N'Bao Binh Co Ltd', N'(+84) 903 346 306', N'binhtranthanhbinh120@gmail.com', N'62 Phu Tho, HCMC', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (2, N'Herbalife Nutrition', N'(+84) 028 483 1919', N'herbalife@gmail.com', N'26 Vo Van Tan, District 3, HCMC', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (3, N'Nutella', N'(+84) 028 329 4385', N'nutella@gmail.com', N'1092 Vo Van Kiet, HCMC', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (4, N'Pink Cereal Company', N'(+84) 058 838 1294', N'pinkcereal@gmail.com', N'394 Nguyen Van Linh, District 7, HCMC', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (5, N'Powow Milk Co.', N'(+84) 028 483 5959', N'powmilk@gmail.com', N'283 Nguyen Huu Tho, District 7, HCMC', N'700000')
 
 SET IDENTITY_INSERT [dbo].[Suppliers] OFF
 /****** ShoppingCartData ******/
@@ -77,7 +78,14 @@ SET ANSI_PADDING OFF
 GO
 SET IDENTITY_INSERT [dbo].[Products] ON
 INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (1, N'EnergyPlus', N'Bao Binh Co.,Ltd', 300.0000, 100, N'Supplement', N'An energy drink a day keep the tire away', 1, 1)
-
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (2, N'Heavy Sugar Milk', N'BB Milk', 5.9900, 300, N'Supplement', N'Milk improve your entire body', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (3, N'Nutella', N'Nutella', 5.9900, 500, N'Supplement', N'Spread the happy!', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (4, N'Pink Cereal', N'Pink Cereal Company', 7.9900, 300, N'Supplement', N'Stays crunchy, even in pink!', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (5, N'Vitamin Pills', N'Bao Binh Co.,Ltd', 21.9900, 350, N'Supplement', N'Helps gain vitamins in meals', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (6, N'Next Coffee', N'Bao Binh Co.,Ltd', 3.9900, 250, N'Supplement', N'Wake up and drink some coffee!', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (7, N'Light Sugar Milk', N'BB Milk', 5.5900, 500, N'Supplement', N'Now with less sugar!', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (8, N'Nutella Honey', N'Nutella', 9.9900, 100, N'Supplement', N'Looks sweet and good', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (9, N'Go Yogurt', N'BB Milk', 4.9900, 300, N'Supplement', N'Very creamy and delicious', 1, 1)
 SET IDENTITY_INSERT [dbo].[Products] OFF
 /****** Orders ******/
 SET ANSI_NULLS ON
