f6081586c54fc90419730e1f22bab6de6bafe0c9
Chocotaco2209 <binhtranthanhbinh120@gmail.com>
2022-12-11 22:24:25 +0700

    Added a drop down menu

--- Data.sql
+++ Data.sql
@@ -1,3 +1,4 @@
+--use [master]
 use [Shop]
 GO
 /****** Suppliers ******/
@@ -27,7 +28,7 @@ INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode
 INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (2, N'Herbalife Nutrition', N'(+84) 028 483 1919', N'herbalife@gmail.com', N'26 Vo Van Tan, District 3, HCMC', N'700000')
 INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (3, N'Nutella', N'(+84) 028 329 4385', N'nutella@gmail.com', N'1092 Vo Van Kiet, HCMC', N'700000')
 INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (4, N'Pink Cereal Company', N'(+84) 058 838 1294', N'pinkcereal@gmail.com', N'394 Nguyen Van Linh, District 7, HCMC', N'700000')
-INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (5, N'Powow Milk Co.', N'(+84) 028 483 5959', N'powmilk@gmail.com', N'283 Nguyen Huu Tho, District 7, HCMC', N'700000')
+INSERT [dbo].[Suppliers] ([SID], [SName], [Phone], [Email], [Address], [Postcode]) VALUES (5, N'BB Milk Co.', N'(+84) 028 483 5959', N'powmilk@gmail.com', N'283 Nguyen Huu Tho, District 7, HCMC', N'700000')
 
 SET IDENTITY_INSERT [dbo].[Suppliers] OFF
 /****** ShoppingCartData ******/
@@ -78,14 +79,15 @@ SET ANSI_PADDING OFF
 GO
 SET IDENTITY_INSERT [dbo].[Products] ON
 INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (1, N'EnergyPlus', N'Bao Binh Co.,Ltd', 300.0000, 100, N'Supplement', N'An energy drink a day keep the tire away', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (2, N'Heavy Sugar Milk', N'BB Milk', 5.9900, 300, N'Supplement', N'Milk improve your entire body', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (3, N'Nutella', N'Nutella', 5.9900, 500, N'Supplement', N'Spread the happy!', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (4, N'Pink Cereal', N'Pink Cereal Company', 7.9900, 300, N'Supplement', N'Stays crunchy, even in pink!', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (5, N'Vitamin Pills', N'Bao Binh Co.,Ltd', 21.9900, 350, N'Supplement', N'Helps gain vitamins in meals', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (6, N'Next Coffee', N'Bao Binh Co.,Ltd', 3.9900, 250, N'Supplement', N'Wake up and drink some coffee!', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (7, N'Light Sugar Milk', N'BB Milk', 5.5900, 500, N'Supplement', N'Now with less sugar!', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (8, N'Nutella Honey', N'Nutella', 9.9900, 100, N'Supplement', N'Looks sweet and good', 1, 1)
-INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (9, N'Go Yogurt', N'BB Milk', 4.9900, 300, N'Supplement', N'Very creamy and delicious', 1, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (2, N'Heavy Sugar Milk', N'BB Milk Co.', 5.9900, 300, N'Supplement', N'Milk improve your entire body', 5, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (3, N'Nutella', N'Nutella', 5.9900, 500, N'Supplement', N'Spread the happy!', 3, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (4, N'Pink Cereal', N'Pink Cereal Company', 7.9900, 300, N'Supplement', N'Stays crunchy, even in pink!', 4, 1)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (5, N'Vitamin Pills', N'Bao Binh Co.,Ltd', 21.9900, 350, N'Supplement', N'Helps gain vitamins in meals', 1, 2)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (6, N'Next Coffee', N'Bao Binh Co.,Ltd', 3.9900, 250, N'Supplement', N'Wake up and drink some coffee!', 1, 2)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (7, N'Light Sugar Milk', N'BB Milk Co.', 5.5900, 500, N'Supplement', N'Now with less sugar!', 5, 2)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (8, N'Nutella Honey', N'Nutella', 9.9900, 100, N'Supplement', N'Looks sweet and good', 3, 3)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (9, N'Go Yogurt', N'BB Milk Co.', 4.9900, 300, N'Supplement', N'Very creamy and delicious', 5, 3)
+INSERT [dbo].[Products] ([PID], [PName], [Brand], [UnitPrice], [UnitsInStock], [Category], [Description], [SID], [ROL]) VALUES (10, N'Mint Chocolate Shake Mix', N'Herbalife Nutrition', 50.9000, 500, N'Supplement', N'Kick-start your healthy eating habits', 2, 3)
 SET IDENTITY_INSERT [dbo].[Products] OFF
 /****** Orders ******/
 SET ANSI_NULLS ON
